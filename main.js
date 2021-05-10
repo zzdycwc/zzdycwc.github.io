4b4ea939ae9569bb2217b7fad8e9456b8b2a339d
zzdycwc <83961059+zzdycwc@users.noreply.github.com>
2021-05-10 21:35:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function(){
+	let mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/火狐01.png') {
+		myImage.setAttribute('src','images/js001.png');
+	} else{
+		myImage.setAttribute('src','images/火狐01.png');
+	}
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {let myName = prompt('请输入你的名字。');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+  }
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+myButton.onclick = function() {
+   setUserName();
+}
+
+
+
+
+
+
+/*关键词src拼写粗心，错误的写为scr,导致重复测试不通过，已更正*/
+//let myImage = document.querySelector('img');
+//myImage.onclick = function() {
+//  let mySrc = myImage.getAttribute('src');
+//  if(mySrc === 'images/火狐01.png') {
+//    myImage.setAttribute('src', 'images/js001.png');
+//  } else {
+//    myImage.setAttribute('src', 'images/火狐01.png');
+//  }
+//}
